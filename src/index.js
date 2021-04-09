9530fa4a199c8257e3a00650ac0891fc4de16629
wendervitor <wendernogueira99@gmail.com>
2021-04-09 20:05:14 -0300

    change of bg depending of status

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const app = require('express')();
 const server = require('http').Server(app);
+
 const { setUpTimer, countdown } = require('./timer');
 
 setUpTimer(server);
 const myTimer = setInterval(countdown,1000);
 
 app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/timer.html');
+    res.sendFile(__dirname + '/public/timer.html');
 });
 
 
--- todos.md
+++ todos.md
@@ -3,8 +3,9 @@
  [X] Make countdown run forever
  [X] Implement a POMODORO
  [X] Refactor folders
+ [X] Change background color depending on status (working or break)
  [ ] Tests
  [ ] Deploy first usage version
  [ ] Implement some bip
  [ ] Implement chat
-  
+ [ ] Implement rooms
